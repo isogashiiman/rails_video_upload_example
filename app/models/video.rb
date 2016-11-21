1878b7657f0eeb6a09a8a9a200b00c33df059505
fahrenhei7 <fahrenhei7lt@gmail.com>
2016-11-21 22:00:51 +0300

    change quotes to single

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,9 +1,8 @@
 class Video < ApplicationRecord
   has_attached_file :video, styles: {
-    medium: { geometry: "640x480", format: 'flv' },
-    thumb: { geometry: "100x100#", format: 'jpg', time: 10 }
+    medium: { geometry: '640x480', format: 'flv' },
+    thumb: { geometry: '100x100#', format: 'jpg', time: 10 }
   }, processors: [:transcoder]
 
   validates_attachment :video, content_type: { content_type: ['video/mp4'] }
-
 end
